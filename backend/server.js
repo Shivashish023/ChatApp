ce5896bc945d738ec3b0ca4d5b5384a13040c0cb
Rival <shivashish.saklani@gmail.com>
2025-02-05 20:38:54 +0530

    backendlinks

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-app-zeta-self.vercel.app/',
     credentials: true,
 };
 
