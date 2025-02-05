36a32e0511e4c7a032ef65865072d802255271a1
Rival <shivashish.saklani@gmail.com>
2025-02-06 00:54:36 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const corsOptions = {
     credentials: true,
 };
 
-app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
