3fb3ba36daa2266e2be53a7742f741b17abca226
Rival <shivashish.saklani@gmail.com>
2026-05-23 11:35:55 +0530

    env fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:import.meta.env.VITE_BACKEND_URL || "http://localhost:3000", // Change this to your backend server URL
+        target:process.env.VITE_BACKEND_URL || "http://localhost:3000", // Change this to your backend server URL
         changeOrigin: true,
        
       },
